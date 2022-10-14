ceb8c1bf99d124f4e576499d105ffeda575ab77d
kshah <kshah@tyro.com>
2022-10-14 19:07:45 +1000

    update ui for recycler view

--- app/src/main/java/com/kartik/grevocab/adapters/display/StockDisplay.kt
+++ app/src/main/java/com/kartik/grevocab/adapters/display/StockDisplay.kt
@@ -3,21 +3,27 @@ package com.kartik.grevocab.adapters.display
 import com.kartik.grevocab.api.pojos.Stock
 import com.kartik.grevocab.utility.CurrencyUtils
 import java.math.BigDecimal
+import java.text.SimpleDateFormat
 import java.util.Date
 
-data class StockDisplay(val stock: Stock, val currencySymbol: String?, val currency: String, val time: String)
+data class StockDisplay(val stock: Stock, val amount: String?, val currency: String, val time: String, val available: String)
 
 fun Stock.toDisplay(currencyUtils: CurrencyUtils) = StockDisplay(
     this,
-    this.currency?.let { currencyUtils.getCurrencySymbol(it) } ?: "$",
+    (
+        (this.currency?.let { currencyUtils.getCurrencySymbol(it) } ?: "$") +
+            this.currentPriceCents?.let { (it / BigDecimal(100)).toPlainString() }
+        ),
     this.currentPriceCents?.let { (it / BigDecimal(100)).toPlainString() } ?: "N/A",
     this.currentPriceTimestamp?.let {
         getDateTimeFromEpocLongOfSeconds(it)
-    } ?: "N/A"
+    } ?: "N/A",
+    this.quantity?.let { "$it available" } ?: ""
 )
 
 fun getEmptyStocks() = StockDisplay(
     Stock("", "No stocks available", null, null, null, null),
+    "",
     "",
     "",
     ""
@@ -25,8 +31,9 @@ fun getEmptyStocks() = StockDisplay(
 
 private fun getDateTimeFromEpocLongOfSeconds(epoc: Long): String? {
     try {
-        val netDate = Date(epoc * 1000)
-        return netDate.toString()
+        val date = Date(epoc * 1000)
+        val formatter = SimpleDateFormat("dd/MM/yyyy HH:mm")
+        return formatter.format(date)
     } catch (e: Exception) {
         return e.toString()
     }
